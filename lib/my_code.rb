a526aa724a51ec65aff0b2450a0fd10cf0e9eca5
Olivia Madden <madden.livi@gmail.com>
2020-01-03 21:12:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    current_element = source_array[i]
+    new_array.push(yield(current_element))
+    i += 1 
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point == nil
+    i = 0 
+  else
+    i = starting_point
+  end
+  while i < source_array.length do 
+    value = nil 
+    current_element = source_array[i]
+    value = yield(current_element)
+    i += 1 
+  end
+  value
+end
+
