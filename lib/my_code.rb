d74abf9e4cb4bd3475111ca8065f8ed26f44bf1e
Olivia Madden <madden.livi@gmail.com>
2020-01-03 21:27:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,6 +11,9 @@ end
 
 def reduce(source_array, starting_point = nil)
   i = 0 
+  if starting_point == nil
+    starting_point= 0 
+  end
   while i < source_array.length do
     current_element = source_array[i]
     value = yield(current_element, starting_point)
